c5c879e73dddaa915fa9131c6a67b06bccb767a8
Daniel Lima <daniellima@quickmobile.com.br>
2018-10-18 14:41:22 -0300

    init

---
+++ mysite/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
+
+# Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post 
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now.order_by('-p'))
